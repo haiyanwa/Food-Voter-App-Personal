6f6577c42b8a99e4b58ec0b4d4103b9f8c506516
shay <shay.nguyen@gmail.com>
2017-07-19 14:56:45 -0700

    impl basic sign in/sign out authentication

--- app/src/main/java/com/android/summer/csula/foodvoter/HomeActivity.java
+++ app/src/main/java/com/android/summer/csula/foodvoter/HomeActivity.java
@@ -1,13 +1,116 @@
 package com.android.summer.csula.foodvoter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class HomeActivity extends AppCompatActivity {
 
-    @Override
+    private static final String ANONYMOUS = "anonymous";
+    private static final int REQUEST_CODE_SIGN_IN = 1;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    private TextView usernameTextView;
+    private String username;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        usernameTextView = (TextView) findViewById(R.id.username_tv);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    username = user.getDisplayName();
+                    usernameTextView.setText(username);
+                } else {
+                    username = ANONYMOUS;
+                    startActivityForResult(
+                        AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(false)
+                            .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.Builder(
+                                                  AuthUI.EMAIL_PROVIDER).build(),
+                                              new AuthUI.IdpConfig.Builder(
+                                                  AuthUI.GOOGLE_PROVIDER).build()))
+                            .build(),
+                        REQUEST_CODE_SIGN_IN);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (authStateListener != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE_SIGN_IN) {
+            if(resultCode== RESULT_OK) {
+                Toast.makeText(this, "Signed in as " + getSignedInUsername(), Toast.LENGTH_LONG).show();
+            } else if(resultCode == RESULT_CANCELED) {
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final int selectedItemId = item.getItemId();
+
+        switch (selectedItemId) {
+            case R.id.sign_out_menu:
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private String getSignedInUsername() {
+        return firebaseAuth.getCurrentUser().getDisplayName();
     }
 }
+
+
