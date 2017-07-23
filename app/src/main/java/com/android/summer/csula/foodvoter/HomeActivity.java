72d7d3b3a83f8b14a7c66268f8ace6c93d9fa9c3
shay <shay.nguyen@gmail.com>
2017-07-22 23:35:42 -0700

    Update home_act UI, add user to db if new

--- app/src/main/java/com/android/summer/csula/foodvoter/HomeActivity.java
+++ app/src/main/java/com/android/summer/csula/foodvoter/HomeActivity.java
@@ -3,16 +3,25 @@ package com.android.summer.csula.foodvoter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
 
@@ -20,44 +29,141 @@ public class HomeActivity extends AppCompatActivity {
 
     private static final String ANONYMOUS = "anonymous";
     private static final int REQUEST_CODE_SIGN_IN = 1;
+    private static final String TAG = HomeActivity.class.getSimpleName();
 
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
 
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference usersDatabaseReference;
+    private ChildEventListener childEventListener;
+
+    private FirebaseUser firebaseUser;
+    private FloatingActionButton addFriendButton;
+
     private TextView usernameTextView;
+
     private String username;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        usernameTextView = (TextView) findViewById(R.id.username_tv);
+        username = ANONYMOUS;
+
+
+        usernameTextView = (TextView) findViewById(R.id.tv_username);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        usersDatabaseReference = firebaseDatabase.getReference().child("users");
 
         firebaseAuth = FirebaseAuth.getInstance();
         authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
+                firebaseUser = firebaseAuth.getCurrentUser();
 
                 if (user != null) {
-                    username = user.getDisplayName();
-                    usernameTextView.setText(username);
+                    onSignedInInitialized(user.getDisplayName());
                 } else {
-                    username = ANONYMOUS;
+                    onSignedOutCleanup();
                     startActivityForResult(
-                        AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setIsSmartLockEnabled(false)
-                            .setAvailableProviders(
-                                Arrays.asList(new AuthUI.IdpConfig.Builder(
-                                                  AuthUI.EMAIL_PROVIDER).build(),
-                                              new AuthUI.IdpConfig.Builder(
-                                                  AuthUI.GOOGLE_PROVIDER).build()))
-                            .build(),
-                        REQUEST_CODE_SIGN_IN);
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(
+                                            Arrays.asList(new AuthUI.IdpConfig.Builder(
+                                                            AuthUI.EMAIL_PROVIDER).build(),
+                                                    new AuthUI.IdpConfig.Builder(
+                                                            AuthUI.GOOGLE_PROVIDER).build()))
+                                    .build(),
+                            REQUEST_CODE_SIGN_IN);
                 }
             }
         };
+
+        addFriendButton = (FloatingActionButton) findViewById(R.id.btn_add_friend);
+        addFriendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    private void onSignedInInitialized(String username) {
+        this.username = username;
+        usernameTextView.setText(username);
+        addNewUserToDatabase();
+        attachDatabaseReadListener();
+    }
+
+    private void addNewUserToDatabase() {
+        usersDatabaseReference.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Log.d(TAG, "Current user id: " + firebaseUser.getUid());
+
+                        if (dataSnapshot.hasChild(firebaseUser.getUid())) {
+                            Log.d(TAG, "existing user has log in");
+                        } else {
+                            Log.d(TAG, "new user has log in; adding user to the database");
+                            usersDatabaseReference.child(firebaseUser.getUid()).setValue(
+                                    new User(firebaseUser.getDisplayName(), firebaseUser.getUid()));
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    private void attachDatabaseReadListener() {
+        if (childEventListener == null) {
+            childEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    User user = dataSnapshot.getValue(User.class);
+                    Log.d(TAG, "name: " + user.getUsername() + ", Id: " + user.getId());
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            };
+            usersDatabaseReference.addChildEventListener(childEventListener);
+        }
+    }
+
+    private void onSignedOutCleanup() {
+        username = ANONYMOUS;
+        usernameTextView.setText("");
+        detachDatabaseReadListener();
+    }
+
+    private void detachDatabaseReadListener() {
+        if (childEventListener != null) {
+            usersDatabaseReference.removeEventListener(childEventListener);
+            childEventListener = null;
+        }
     }
 
     @Override
@@ -73,16 +179,18 @@ public class HomeActivity extends AppCompatActivity {
         if (authStateListener != null) {
             firebaseAuth.removeAuthStateListener(authStateListener);
         }
+        detachDatabaseReadListener();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == REQUEST_CODE_SIGN_IN) {
-            if(resultCode== RESULT_OK) {
-                Toast.makeText(this, "Signed in as " + getSignedInUsername(), Toast.LENGTH_LONG).show();
-            } else if(resultCode == RESULT_CANCELED) {
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Signed in as " + getSignedInUsername(),
+                        Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
                 finish();
             }
         }
