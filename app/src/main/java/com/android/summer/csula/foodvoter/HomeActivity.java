83b7e207eb2efc690e4fc87d4f57ea71daca0daa
shay <shay.nguyen@gmail.com>
2017-07-23 02:20:27 -0700

    Add indicator, indicate client online/offline:
    next to the username will be an image.
    when the uesr is online = drawable.presence.online
    when the user is offline = drawable.presence.offline

--- app/src/main/java/com/android/summer/csula/foodvoter/HomeActivity.java
+++ app/src/main/java/com/android/summer/csula/foodvoter/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.android.summer.csula.foodvoter;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +12,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -86,6 +88,24 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        DatabaseReference connectedRef = FirebaseDatabase.getInstance().getReference(".info/connected");
+        connectedRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean connected = dataSnapshot.getValue(Boolean.class);
+                if(connected) {
+                    Drawable img = getResources().getDrawable(android.R.drawable.presence_online);
+                    usernameTextView.setCompoundDrawablesWithIntrinsicBounds(null, null, img, null);
+                } else {
+                    Drawable img = getResources().getDrawable(android.R.drawable.presence_offline);
+                    usernameTextView.setCompoundDrawablesWithIntrinsicBounds(null, null, img, null);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 
     private FirebaseAuth.AuthStateListener setupAuthStateListener() {
