34c6e7adfc258a4547e017c0c37d1108ffc0109c
haiyanwa <haiyanwa@gmail.com>
2017-07-25 15:09:58 -0700

    add submit button and switch listener

--- app/src/main/java/com/android/summer/csula/foodvoter/ListActivity.java
+++ app/src/main/java/com/android/summer/csula/foodvoter/ListActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
-public class ListActivity extends AppCompatActivity implements RVoteAdapter.ListItemClickListener {
+public class ListActivity extends AppCompatActivity implements RVoteAdapter.ListItemClickListener, RVoteAdapter.SwitchListener {
 
     private RVoteAdapter rVoteAdapter;
     private RecyclerView rVoteRecyclerView;
+    private Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +24,34 @@ public class ListActivity extends AppCompatActivity implements RVoteAdapter.List
 
         rVoteRecyclerView.setHasFixedSize(true);
 
-        rVoteAdapter = new RVoteAdapter(Restaurant.generateRestarantList(), this);
+        rVoteAdapter = new RVoteAdapter(Restaurant.generateRestarantList(), this, this);
         rVoteRecyclerView.setAdapter(rVoteAdapter);
     }
 
     @Override
     public void onListItemClick(int clickedItemIndex) {
-
+        if(mToast != null){
+            mToast.cancel();
+        }
+        String toastMessage = "Item #" + clickedItemIndex + "clicked. ";
+        mToast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
+        mToast.show();
     }
+
+    @Override
+    public void onSwitchSwiped(int swipedItemIndex, boolean swiped) {
+        if(mToast != null){
+            mToast.cancel();
+        }
+        String toastMessage = "";
+        if(swiped){
+            toastMessage = "Item #" + swipedItemIndex + " swipted on. ";
+        }else{
+            toastMessage = "Item #" + swipedItemIndex + " swipted off. ";
+        }
+
+        mToast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
+        mToast.show();
+    }
+
 }
--- app/src/main/java/com/android/summer/csula/foodvoter/RVoteAdapter.java
+++ app/src/main/java/com/android/summer/csula/foodvoter/RVoteAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -18,67 +20,106 @@ public class RVoteAdapter extends RecyclerView.Adapter<RVoteAdapter.ViewHolder>{
 
     final private ListItemClickListener mOnClickListener;
 
+    private SwitchListener switchListener;
+
     private static int viewHolderCount;
 
     private int mNumberItems;
 
     private List<Restaurant> mChoiceData;
 
+    private final int ListItem = 0;
+    private final int EndOfList= 1;
+
+    private String TAG = "RVoteAdapter";
+
     public interface ListItemClickListener{
         void onListItemClick(int clickedItemIndex);
     }
 
+    public interface SwitchListener{
+        void onSwitchSwiped(int swipedItemIndex, boolean swiped);
+    }
 
-    public RVoteAdapter(List<Restaurant> restaurants, ListItemClickListener listener) {
+
+    public RVoteAdapter(List<Restaurant> restaurants, ListItemClickListener listener, SwitchListener swListener) {
         mChoiceData = restaurants;
         mOnClickListener = listener;
+        switchListener = swListener;
 
     }
 
     @Override
+    public int getItemViewType(int position) {
+        return (position == mChoiceData.size()) ? EndOfList : ListItem;
+    }
+
+    @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
 
-        View view = inflater.inflate(R.layout.list_item, parent, false);
+        if(viewType == EndOfList){
+            view = inflater.inflate(R.layout.vote_submit_btn, parent, false);
+        }else{
+            view = inflater.inflate(R.layout.list_item, parent, false);
+        }
         ViewHolder holder = new ViewHolder(view);
         return holder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Restaurant restaurant = mChoiceData.get(position);
-        holder.choiceItemView.setText(restaurant.getName());
-
-        Switch vSwitch = holder.voteSwitch;
-        vSwitch.setTextOn("Yes");
-        vSwitch.setTextOff("No");
-        vSwitch.setChecked(false);
-
+        holder.bind(holder, position);
     }
 
     @Override
     public int getItemCount() {
         if (null == mChoiceData) return 0;
-        return mChoiceData.size();
+        return mChoiceData.size() + 1;
     }
-
 
     class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         public TextView choiceItemView;
         public Switch voteSwitch;
+        public Button voteButton;
+        int index;
 
         public ViewHolder(View view){
             super(view);
             choiceItemView = (TextView) view.findViewById(R.id.rv_choice_item);
             voteSwitch = (Switch) view.findViewById(R.id.rv_vote_switch);
+            voteButton = (Button) view.findViewById(R.id.rv_vote_btn);
             view.setOnClickListener(this);
+        }
+
+        public void bind(ViewHolder holder, int position){
+            if(position < mChoiceData.size()){
+                Restaurant restaurant = mChoiceData.get(position);
+                choiceItemView.setText(restaurant.getName());
+
+                voteSwitch.setTextOn("Yes");
+                voteSwitch.setTextOff("No");
+                voteSwitch.setChecked(false);
+
+                index = position;
+
+                holder.voteSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+
+                    @Override
+                    public void onCheckedChanged(CompoundButton compoundButton, boolean swiped) {
+                        switchListener.onSwitchSwiped(index, swiped);
+                    }
+                });
+            }
         }
 
         @Override
         public void onClick(View v) {
-
+            int pos = getAdapterPosition();
+            mOnClickListener.onListItemClick(pos);
         }
     }
 
