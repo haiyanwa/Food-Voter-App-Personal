dc1413c08c5951f8e6a4d2c4d7836b780ee9f173
shay <shay.nguyen@gmail.com>
2017-08-04 23:51:26 -0700

    impl push notification, active when poll created

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,37 @@
 const functions = require('firebase-functions');
-const admin     = require('firebase-admin');
+const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-let activityTracker          = 0;
-// Update the date whenever a poll is created
-exports.udateLastPollCreated = functions.database.ref("/polls/{id}")
-    .onCreate(event => {
-      activityTracker++;
-      return admin.database().ref('activity_tracker').set(activityTracker);
-    });
 
+const LOREM_IPSUM = 'Nullam hendrerit lobortis egestas. Donec dignissim posuere nulla.' +
+    'Suspendisse posuere tristique nisl in tincidunt. ' +
+    'Integer et purus ut odio interdum consectetur. Ut tincidunt in lectus at ultrices.';
+
+// Send a Heads-Up Notification to all the invited voters whenever
+// a poll is created
+exports.messageVoters = functions.database.ref("/polls/{id}/voters")
+    .onCreate(event => {
+        const votersRef = event.data.ref;
+
+        const payload = {
+            data: {
+                title: "Lorem Ipsum",
+                body: LOREM_IPSUM
+            }
+        };
+
+        const options = {
+            priority: "high",
+            timeToLive: 60 * 60 * 24
+        };
+
+        votersRef.once('value')
+            .then(dataSnapshot => {
+
+                dataSnapshot.forEach(child => {
+                    const voter = child.val();
+                    console.log("username: " + voter.username);
+                    admin.messaging().sendToDevice(voter.token, payload, options);
+                });
+            })
+    });
